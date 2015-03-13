cd5c664e188c23dde196cb5602b8504216a6bb53
Elvis Nuñez <NSElvis@users.noreply.github.com>
2015-03-13 14:20:02 +0100

    Fix weird stars

--- Source/HYPScatterLabel.h
+++ Source/HYPScatterLabel.h
@@ -4,8 +4,8 @@
 @interface HYPScatterLabel : NSObject
 
 @property (nonatomic) UIColor *textColor;
-@property (nonatomic) NSString* text;
-@property (nonatomic) UIFont* font;
+@property (nonatomic) NSString *text;
+@property (nonatomic) UIFont *font;
 @property (assign) BOOL autoSizeText;
 
 - (instancetype)initWithText:(NSString *)text textColor:(UIColor *)textColor font:(UIFont *)font autosize:(BOOL)autoSize;
