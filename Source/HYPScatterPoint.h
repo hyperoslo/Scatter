4296029b40829731ae8280d59e6dcd9e772fdf53
Elvis Nuñez <NSElvis@users.noreply.github.com>
2015-03-13 14:23:43 +0100

    Update HYPScatterPoint.h

--- Source/HYPScatterPoint.h
+++ Source/HYPScatterPoint.h
@@ -2,14 +2,12 @@
 @import Foundation;
 
 @interface HYPScatterPoint : NSObject
-{
-    
-}
+
 @property (nonatomic) UIColor *fillColor;
 @property (nonatomic) UIColor *strokeColor;
 @property (assign) CGFloat x;
 @property (assign) CGFloat y;
 
-- (id)initWithValues:(UIColor*)fillColor strokeColor:(UIColor*)strokeColor x:(CGFloat)x y:(CGFloat)y;
+- (instancetype)initWithValues:(UIColor *)fillColor strokeColor:(UIColor *)strokeColor x:(CGFloat)x y:(CGFloat)y;
 
 @end
